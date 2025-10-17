a96a6c0303d8fe8070e2842150162d53a421a38a
Lee Kerley <ld_kerley@icloud.com>
2025-10-17 14:17:46 -0700

    attempt to lift github ccache implementation from OIIO

---
+++ .github/scripts/ci-startup.bash
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# Copyright Contributors to the OpenImageIO project.
+# SPDX-License-Identifier: Apache-2.0
+# https://github.com/AcademySoftwareFoundation/OpenImageIO
+
+# This script is run when CI system first starts up.
+# Since it sets many env variables needed by the caller, it should be run
+# with 'source', not in a separate shell.
+
+# Environment variables we always need
+export PATH=/usr/local/bin/_ccache:/usr/lib/ccache:$PATH
+export USE_CCACHE=${USE_CCACHE:=1}
+export CCACHE_CPP2=
+export CCACHE_DIR=$HOME/.ccache
+export CCACHE_COMPRESSION=yes
+if [[ "$(which ccache)" != "" ]] ; then
+    # Try to coax dependency building into also using ccache
+    export CMAKE_CXX_COMPILER_LAUNCHER="ccache"
+    export CMAKE_C_COMPILER_LAUNCHER="ccache"
+    ccache -z
+fi
+mkdir -p $CCACHE_DIR
